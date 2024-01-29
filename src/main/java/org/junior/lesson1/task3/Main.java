f54343cb17fb5e32160a9d08cbacea55fef5ea44
Vadim <orc225@mail.ru>
2024-01-29 22:27:29 +0300

    first commit

--- src/main/java/org/junior/lesson1/task3/Main.java
+++ src/main/java/org/junior/lesson1/task3/Main.java
@@ -1,2 +1,30 @@
-package org.junior.lesson1.task3;public class Main {
+package org.junior.lesson1.task3;
+
+import ru.geekbrains.junior.lesson1.task1.Book;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(
+                3, 4, 6, -5, 5, 7, 8, 10, -14
+        );
+// вариант из списка с промежуточным результатом
+        List<Integer> evenNumbers = numbers
+                .stream()
+                .filter(num -> num % 2 == 0)
+                .toList();
+        System.out.println(evenNumbers);
+        double av = evenNumbers.stream().mapToInt(i -> i).average().getAsDouble();
+        System.out.println(av);
+
+// вариант из массива сразу среднее одной строкой
+        int[] array = {3, 4, 6, -5, 5, 7, 8, 10, -14};
+        double av2 = Arrays.stream(array).filter(num -> num % 2 == 0).mapToLong(i -> i).average().getAsDouble();
+        System.out.println(av2);
+    }
+
 }
